5ada4223407e0a1d5a9f4fe058a7470ada2e1d12
tom <tom@the-cloud>
2021-04-08 16:52:32 +0000

    deps bump

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject helmet "0.1.0-SNAPSHOT"
+(defproject helmet "0.1.1-SNAPSHOT"
   :description "Rules Language for Defining Stochastic
                 Demand Future Generators"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [spork "0.2.1.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [spork "0.2.1.4-SNAPSHOT"]])
